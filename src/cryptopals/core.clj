c2ba87986a8648d12528276727ead173ba3e2b75
Andreas Arnold <andreas@featureenvy.com>
2016-04-13 17:57:59 +0200

    Set 1 Challenge 2: XORing with clojure.

--- src/cryptopals/core.clj
+++ src/cryptopals/core.clj
@@ -1,61 +1,9 @@
 (ns cryptopals.core
   (:gen-class))
 
-(defn generate-binary-triple->ascii-mapping
-  [start-idx [start-char end-char]]
-  (let [char-range (range (int start-char) (+ 1 (int end-char)))]
-    (zipmap (iterate inc start-idx) (map char char-range))))
-
-(defn generate-mapping-table []
-  (merge (generate-binary-triple->ascii-mapping 0 [\A \Z])
-         (generate-binary-triple->ascii-mapping 26 [\a \z])
-         (generate-binary-triple->ascii-mapping 52 [\0 \9])
-         {62 \+ 63 \\}))
-
-(def ^:const mapping-table (generate-mapping-table))
-
-(defn- byte->binary-str
-  [val]
-  (->> val
-       Integer/toBinaryString
-       Integer/parseInt
-       (format "%08d")))
-
-(defn- partition-to-6-complements
-  [byte-array]
-  (->> byte-array
-       (partition 6 6 (repeat \0))
-       (map clojure.string/join)))
-
-(defn my-byte-array->binary-str
-  [byte-array]
-  (clojure.string/join (map byte->binary-str byte-array)))
-
-(defn my-byte-array->base64-str
-  [input]
-  (->> input
-       my-byte-array->binary-str
-       partition-to-6-complements
-       (map #(Integer/parseInt % 2))
-       (map #(get mapping-table %))
-       clojure.string/join))
-
-(defn my-hex-str->byte-array
-  [hex-str]
-  (->> hex-str
-       (partition 2 2 (repeat \0))
-       (map clojure.string/join)
-       (map #(Byte/parseByte % 16))))
-
-(defn my-hex->base64
-  [input]
-  (-> input
-      my-hex-str->byte-array
-      my-byte-array->base64-str))
-
 (defn hex-str->byte-array
   [hex-str]
-  (vec (.toByteArray (BigInteger. hex-str 16))))
+  (.toByteArray (BigInteger. hex-str 16)))
 
 (defn byte-array->binary-str
   [bytes]
@@ -63,6 +11,10 @@
     (-> byte-array
         (BigInteger.)
         (.toString 2))))
+
+(defn hex-str->binary-str
+  [hex-str]
+  (byte-array->binary-str (hex-str->byte-array hex-str)))
 
 (defn byte-array->base64-str
   [bytes]
@@ -76,6 +28,12 @@
   (-> hex-str
       hex-str->byte-array
       byte-array->base64-str))
+
+(defn xor
+  [hex-input hex-key]
+  (let [binary-input (hex-str->byte-array hex-input)
+        binary-key (hex-str->byte-array hex-key)]
+    (.toString (.xor (BigInteger. binary-input) (BigInteger. binary-key)) 16)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/cryptopals/core_test.clj
+++ test/cryptopals/core_test.clj
@@ -2,39 +2,13 @@
   (:require [clojure.test :refer :all]
             [cryptopals.core :refer :all]))
 
-(deftest mapping-table-test
-  (testing "Returns correct values for upper case characters"
-    (is (= \A (get mapping-table 0)))
-    (is (= \Z (get mapping-table 25))))
-  (testing "Returns correct values for lower case characters"
-    (is (= \a (get mapping-table 26)))
-    (is (= \z (get mapping-table 51))))
-  (testing "Returns correct value for digit characters"
-    (is (= \0 (get mapping-table 52)))
-    (is (= \9 (get mapping-table 61))))
-  (testing "Returns correct value for special characters"
-    (is (= \+ (get mapping-table 62)))
-    (is (= \\ (get mapping-table 63)))))
-
-(deftest my-hex-str->byte-array-test
-  (testing "Converts hex strings to byte arrays"
-    (is (= [74] (my-hex-str->byte-array "4A")))))
-
-(deftest my-byte-array->binary-str-test
-  (testing "Converts byte array to binary string"
-    (is (= "0100101001010010" (my-byte-array->binary-str [74 82])))))
-
-(deftest my-byte-array->base64-str-test
-  (testing "Converts byte array to base64 string"
-    (is (= "SSc" (my-byte-array->base64-str [73 39])))))
-
-(deftest my-str->base64-test
-  (testing "Converts from str to base64"
-    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" (my-hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))))
-
 (deftest hex-str->byte-array-test
   (testing "Converts a hex string into a byte array"
     (is (= [74] (vec (hex-str->byte-array "4A"))))))
+
+(deftest hex-str->binary-str-test
+  (testing "Converts a hex string into a binary string"
+    (is (= "1001010" (hex-str->binary-str "4A")))))
 
 (deftest byte-array->binary-str-test
   (testing "Converts byte array to binary string"
@@ -46,6 +20,8 @@
 
 (deftest str->base64-test
   (testing "Converts from str to base64"
-    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" (hex->base64 "49276d206b696c6c696e6720796f7572206272616;; 96e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-")))))
+    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))))
 
+(deftest xor-test
+  (testing "XORing two hex encoded strings"
+    (is (= "746865206b696420646f6e277420706c6179" (xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965")))))
